29736071df38ec1f563886d4108545af7313a72d
marwane <marw0619@stud.kea.dk>
2022-04-22 11:18:18 +0200

    session validation adlinPanel

--- admin_get.py
+++ admin_get.py
@@ -1,7 +1,21 @@
-from bottle import get, view
+from bottle import get, view, response,redirect, request
 import g
+import jwt
 
 @get("/adminPanel")
 @view("admin")
 def _():
+  if len(g.SESSIONS) < 1 :
+    response.status = 400
+    return redirect("/login?error=invalidS")
+  if not (request.get_cookie("jwt")) :
+    response.status = 400
+    return redirect("/login?error=invalidS")
+  encoded_jwt = request.get_cookie("jwt")
+  user_info = jwt.decode(encoded_jwt ,  "theKey" , algorithms="HS256") 
+  if not (user_info == "admin1"):
+    return redirect("/login?error=invalidS")
+
+
+
   return dict(tweets = g.TWEETS )
