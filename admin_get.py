e76d9c2d633af23ecc36a57bfff6f80d8c7f8671
marwane <marw0619@stud.kea.dk>
2022-04-22 11:34:24 +0200

    fix of session validation admin Panel

--- admin_get.py
+++ admin_get.py
@@ -12,8 +12,9 @@ def _():
     response.status = 400
     return redirect("/login?error=invalidS")
   encoded_jwt = request.get_cookie("jwt")
-  user_info = jwt.decode(encoded_jwt ,  "theKey" , algorithms="HS256") 
-  if not (user_info == "admin1"):
+  user_info = jwt.decode(encoded_jwt ,  "theKey" , algorithms="HS256")
+
+  if not (user_info["user_id"] == "admin1"):
     return redirect("/login?error=invalidS")
 
 
